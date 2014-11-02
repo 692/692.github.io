3146188d3cc35587df9186c635b002735f869019
692 <6889692@gmail.com>
2014-11-02 23:32:32 +0800

    g

--- marked.js
+++ marked.js
@@ -3,7 +3,21 @@
  * Copyright (c) 2011-2014, Christopher Jeffrey. (MIT Licensed)
  * https://github.com/chjj/marked
  */
-
+$(document).ready(function(){
+    // 页面向下滚动超过300px时按钮显示，否则隐藏
+    $(window).scroll(function(){
+    if($(this).scrollTop()>300){
+    $('.back-to-top').fadeIn(100);
+    } else {
+    $('.back-to-top').fadeOut(100);
+    }
+    });
+    // 点击按钮，向上动画滚动
+    $('.back-to-top').click(function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:0},500);
+    })
+});
 ;(function() {
 
 /**
